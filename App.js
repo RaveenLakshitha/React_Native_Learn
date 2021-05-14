516ddcea0cf580132a3be88fd8da7a9e14a3b7cd
Raveen_lw <49742733+RaveenLakshitha@users.noreply.github.com>
2021-05-14 19:46:40 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,43 +1,21 @@
 import React, { useState } from "react";
-import { Button, FlatList, StyleSheet, Text, TextInput, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import GoalInput from "./Components/GoalInput";
+import GoalItem from "./Components/GoalItem";
 
 export default function App() {
-  const [enteredText, setenteredText] = useState('');
+
   const [submitText, setsubmitText] = useState([]);
 
-  const textEnter = (text) => {
-    setenteredText(text);
-  }
-
-  const textShow = () => {
+  const textShow = (enteredText) => {
     setsubmitText(textToShow => [...textToShow, { id: Math.random().toString(), value: enteredText }]);
     /* setsubmitText(textToShow => [...textToShow, { key: Math.random().toString(), value: enteredText }]); */
   }
 
   return (
     <View style={styles.screen}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-          padding: 20
-        }}
-      >
-        <TextInput
-          placeholder="Add Text"
-          onChangeText={textEnter}
-          value={enteredText}
-          style={{
-            width: "60%",
-            borderColor: "black",
-            borderWidth: 3,
-            padding: 10
-          }}
-        />
-        <Button title="Enter" onPress={textShow} />
-      </View>
-      <FlatList keyExtractor={(item, index) => item.id} data={submitText} renderItem={(itemData) => (<View style={styles.listItem}><Text>{itemData.item.value}</Text></View>)}>
+      <GoalInput onAddGoal={textShow} />
+      <FlatList keyExtractor={(item, index) => item.id} data={submitText} renderItem={itemData => (<GoalItem title={itemData.item.value} />)}>
       </FlatList>
 
       <View style={styles.inputContainer}>
